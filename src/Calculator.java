5c420764f33d757da62e24920f39a21f7f023b30
Lalith <lalithchalla111@gmail.com>
2023-10-17 20:17:19 +0530

    Learnt about swing library and with using references could code the app but still changes are need to eb made

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,23 +1,93 @@
+/**
+ * Author:Challa Saraswathi Lalith
+ * Date:01-10-2023
+ * Project:Calculator App
+ */
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 public class Calculator extends JFrame implements ActionListener {
-    private JTextField display;
-    private JButton[] buttons;
-    private String[] buttonLabels = {
+    private final JTextField display;
+    private final JButton[] buttons;
+    private final String[] buttonLabels = {
             "7", "8", "9", "/",
             "4", "5", "6", "*",
             "1", "2", "3", "-",
             "0", "C", "=", "+"
     };
+    private double num1, num2, result;
+    private char operator;
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new Calculator());
+    public Calculator() {
+        setTitle("Calculator App");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 400);
+        setLayout(new BorderLayout());
+
+        display = new JTextField();
+        display.setHorizontalAlignment(JTextField.RIGHT);
+        display.setEditable(false);
+        add(display, BorderLayout.NORTH);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(4, 4));
+
+        buttons = new JButton[buttonLabels.length];
+        for (int i = 0; i < buttonLabels.length; i++) {
+            buttons[i] = new JButton(buttonLabels[i]);
+            buttons[i].addActionListener(this);
+            buttonPanel.add(buttons[i]);
+        }
+
+        add(buttonPanel, BorderLayout.CENTER);
+
+        setVisible(true);
     }
-
-    @Override
     public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
 
+        if (command.matches("[0-9]")) {
+            display.setText(display.getText() + command);
+        } else if (command.matches("[+\\-*/]")) {
+            num1 = Double.parseDouble(display.getText());
+            operator = command.charAt(0);
+            display.setText("");
+        } else if (command.equals("C")) {
+            display.setText("");
+        } else if (command.equals("=")) {
+            num2 = Double.parseDouble(display.getText());
+
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                    } else {
+                        display.setText("Error");
+                        return;
+                    }
+                    break;
+            }
+            int res;
+            if(result==(int)result){
+                res=(int)result;
+                display.setText(Integer.toString((int) res));
+            }
+            else
+                display.setText(Double.toString(result));
+        }
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Calculator::new);
     }
 }
